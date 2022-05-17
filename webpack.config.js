29453d09410b8db23c054df49ea6e7fc94ceb014
i1oy <luyuhang1991@gmail.com>
2022-05-18 00:52:20 +0800

    Add images support

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,28 @@ if (process.env.NODE_ENV === "production") {
 }
 module.exports = {
     mode: mode,
+    output: {
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
     module: {
         rules: [
             {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset",
+                // type: "asset/inline",
+                // parser: {
+                //     dataUrlCondition: {
+                //         maxSize: 30 * 1024
+                //     }
+                // }
+            },
+            {
                 test: /\.(s[ac]|c)ss$/i,
                 use: [
-                    MiniCSSExtractPlugin.loader,
+                    {
+                        loader: MiniCSSExtractPlugin.loader,
+                        options: { publicPath: "" },
+                    },
                     "css-loader",
                     "postcss-loader",
                     "sass-loader"
